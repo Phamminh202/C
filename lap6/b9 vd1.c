c5a19d900d5cbca49ebd735a026f7d2faf1173cd
Phamminh202 <73330638+Phamminh202@users.noreply.github.com>
2020-10-27 23:28:36 +0700

    lap6

---
+++ lap6/b9 vd1.c
@@ -0,0 +1,11 @@
+/*day la chuong trinh minh hoa vong lap for trong chuong trinh*/
+#include<stdio.h>
+main()
+{
+	int count;
+	printf("\tThis is a\n");
+	for(count=1;count<=6;count++)
+	printf("\n\t\tnice");
+	printf("\n\t\tworld.\n");
+}
+
---
+++ lap6/b9 vd10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num;
+	label1:
+		printf("\nEnter a number (1):");
+		scanf("%d",&num);
+		if(num==1)
+		goto Test;
+		else
+		goto label1;
+		Test:
+			printf("All done...");
+}
